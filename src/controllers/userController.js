65c6b79280d87164b96a7d17c041a1ac283500ca
Bosnan <brayancordova9900@gmail.com>
2023-12-04 09:47:40 -0600

    adding more routes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,9 @@
 const formLogin = (req, res) => {
-    res.render('auth/login', {
-        authenticated: false,
-    });
+    res.render('auth/login', {});
 };
 
-export { formLogin };
+const formRegister = (req, res) => {
+    res.render('auth/register', {});
+};
+
+export { formLogin, formRegister };
